530636b12e2f27f8ac86e1caa55adde707973f8a
Kolakanmi Apanisile <Kolakanmi.Apanisile@sterling.ng>
2022-03-04 21:44:12 +0100

    create wallet if empty

--- repository/wallet.go
+++ repository/wallet.go
@@ -13,7 +13,7 @@ import (
 )
 
 type IWalletRepository interface {
-	Create(ctx context.Context) (string, error)
+	Create(ctx context.Context, amount float64) (*model.Wallet, error)
 	Get(ctx context.Context) (*model.Wallet, error)
 	UpdateOrCreate(ctx context.Context, amount float64) (*model.Wallet, error)
 }
@@ -24,51 +24,49 @@ type WalletRepository struct {
 
 var ErrInsufficientBalance = errors.New("insufficient balance")
 
+var (
+	getStatement = `
+		SELECT id, created_at, updated_at, balance FROM kola_wallets 
+		where deleted_at is null
+	`
+	updateStatement = `
+		UPDATE kola_wallets SET balance = balance + $1, updated_at = $2 RETURNING balance, updated_at;
+	`
+	createStatement = `
+		INSERT INTO kola_wallets (id, created_at, updated_at, balance)
+		VALUES ($1, $2, $3, $4)
+	`
+)
+
 func NewWalletRepository(db *sql.DB) *WalletRepository {
 	return &WalletRepository{db: db}
 }
 
-func (w *WalletRepository) Create(ctx context.Context) (string, error) {
+func (w *WalletRepository) Create(ctx context.Context, amount float64) (*model.Wallet, error) {
 	timeNow := utils.TimeNow()
 	id := uuid.New()
-	statement := `
-		INSERT INTO kola_wallets (id, created_at, updated_at, balance)
-		VALUES ($1, $2, $3, $4)
-	`
-	_, err := w.db.ExecContext(ctx, statement, id, timeNow, timeNow, 0.0)
+
+	_, err := w.db.ExecContext(ctx, createStatement, id, timeNow, timeNow, amount)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return id, nil
+	return &model.Wallet{Base: model.Base{ID: id, CreatedAt: timeNow, UpdatedAt: timeNow}, Balance: amount}, nil
 }
 
 func (w *WalletRepository) Get(ctx context.Context) (*model.Wallet, error) {
-	query := `
-		SELECT id, created_at, updated_at, balance FROM kola_wallets 
-		where deleted_at is null
-	`
-	row := w.db.QueryRowContext(ctx, query)
+	row := w.db.QueryRowContext(ctx, getStatement)
 	var wallet model.Wallet
 	err := row.Scan(&wallet.ID, &wallet.CreatedAt, &wallet.UpdatedAt, &wallet.Balance)
 	if err != nil {
+		if err == sql.ErrNoRows {
+			return w.Create(ctx, 0)
+		}
 		return nil, err
 	}
 	return &wallet, nil
 }
 
 func (w *WalletRepository) UpdateOrCreate(ctx context.Context, amount float64) (*model.Wallet, error) {
-	id := uuid.New()
-	getStatement := `
-		SELECT id, created_at, updated_at, balance FROM kola_wallets 
-		where deleted_at is null
-	`
-	updateStatement := `
-		UPDATE kola_wallets SET balance = balance + $1, updated_at = $2 RETURNING balance, updated_at;
-	`
-	createStatement := `
-		INSERT INTO kola_wallets (id, created_at, updated_at, balance)
-		VALUES ($1, $2, $3, $4)
-	`
 	row := w.db.QueryRowContext(ctx, getStatement)
 	var wallet model.Wallet
 	err := row.Scan(&wallet.ID, &wallet.CreatedAt, &wallet.UpdatedAt, &wallet.Balance)
@@ -91,12 +89,5 @@ func (w *WalletRepository) UpdateOrCreate(ctx context.Context, amount float64) (
 		return nil, ErrInsufficientBalance
 	}
 	//if no row, create wallet record
-	timeNow := utils.TimeNow()
-	_, err = w.db.ExecContext(ctx, createStatement, id, timeNow, timeNow, amount)
-	if err != nil {
-		return nil, err
-	}
-
-	return &model.Wallet{Base: model.Base{ID: id, CreatedAt: timeNow, UpdatedAt: timeNow}, Balance: amount}, nil
-
+	return w.Create(ctx, amount)
 }
